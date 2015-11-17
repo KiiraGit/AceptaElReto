b4a3e0c228a36155dbb88ef214ba191f04e2a62c
alumno <alumno@alumno-desktop>
2015-11-17 10:20:49 +0100

    kjl

--- AceptaElReto/src/Ejercicios/_105Ventas.java
+++ AceptaElReto/src/Ejercicios/_105Ventas.java
@@ -11,8 +11,9 @@ public class _105Ventas
 		Scanner tec = new Scanner(System.in).useLocale(Locale.US);
 		ArrayList<String> salida = new ArrayList();
 		
-		double ingresos = 0, cont = 0, mediaSem = 0;
-		String diaMax, diaMin, domMax;
+		double ingresos = 0, mediaSem = 0, diaMaxC = 0, diaMinC = 10000000;
+		int cont = 0;
+		String diaMax = "", diaMin = "", domMax;
 		
 		while (ingresos != -1 && cont == 0 || cont > 0)
 		{
@@ -20,10 +21,67 @@ public class _105Ventas
 			if (ingresos != -1)
 			{
 				mediaSem += ingresos;
+				if (ingresos > diaMaxC) 
+				{
+					diaMaxC = ingresos;
+					diaMax = diaSelect(cont);
+				}
+				else if (ingresos < diaMinC)
+				{
+					diaMinC = ingresos;
+					diaMin = diaSelect(cont);
+				}
+				else
+				{
+					if (ingresos == diaMaxC)
+					{
+						diaMax = diaSelect(15);
+					}
+					if (ingresos == diaMinC)
+					{
+						diaMin = diaSelect(15);
+					}
+				}
 			}
 			cont++;
-			if (cont > 6)
+			if (cont == 6)
+			{
 				cont = 0;
+				mediaSem /= 6;
+				if (mediaSem > ingresos)
+				{
+					domMax = "SI";
+				}
+				else domMax = "NO";
+				salida.add(diaMax + diaMin + domMax);
+				diaMinC = 10000000;
+				diaMaxC = 0;
+				diaMax = "";
+				diaMin = "";
+				domMax = "";
+				mediaSem = 0;
+			}
+		}
+	}
+	
+	public static String diaSelect(int dia)
+	{
+		switch (dia)
+		{
+			case 0:
+				return "MARTES";
+			case 1:
+				return "MIERCOLES";
+			case 2:
+				return "JUEVES";
+			case 3:
+				return "VIERNES";
+			case 4:
+				return "SABADO";
+			case 5:
+				return "DOMINGO";
+			default:
+				return "EMPATE";
 		}
 	}
 
